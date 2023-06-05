0420c573ae0c875d9940264f6e197ff5e2d86e01
degen007 <135591321+degen007@users.noreply.github.com>
2023-06-05 13:56:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit
 
 streamlit.title('my parents new healthy dinner')
 
-streamlit.header('Breakfast Menu:')
-streamlit.text('Omega3 and blueberry oats')
-streamlit.text('boiled eggs')
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
