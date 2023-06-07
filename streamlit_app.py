1675fe4cc1c5dcaa0b5f54710414afc4113921b8
degen007 <135591321+degen007@users.noreply.github.com>
2023-06-07 15:38:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-import pandas
-import requests
-import snowflake.connector
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
@@ -33,20 +31,29 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+      streamlit.error("please select a fruit to get info")
+   else:
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       streamlit.dataframe(fruityvice_normalized)
+      
+      
+  #streamlit.write('The user entered ', fruit_choice)
 
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 steamlit.stop()
 import snowflake.connector
